91576bdc87ac10faeeab0b3c30b87de0bc53c31f
Jc <jordan.clain@etu.univ-amu.fr>
2018-11-21 21:35:22 +0100

    Dernière modif

--- partage/iftun.c
+++ partage/iftun.c
@@ -9,14 +9,7 @@ int tun_alloc(char *dev)
     perror("alloc tun");
     exit(-1);
   }
-
   memset(&ifr, 0, sizeof(ifr));
-
-  /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
-   *        IFF_TAP   - TAP device
-   *
-   *        IFF_NO_PI - Do not provide packet information
-   */
   ifr.ifr_flags = IFF_TUN ;
   if( *dev )
     strncpy(ifr.ifr_name, dev, IFNAMSIZ);
@@ -62,41 +55,3 @@ int createAndSetTun(char * tun_name, char * address){
 
   return tunfd;
 }
-/*
-int main(int narg, char** argv){
-
-  if(narg != 3){printf("Usage::%s tunnel_name address\n", argv[0]); exit(1);}
-
-  int tunfd = createAndSetTun(argv[1], argv[2]);
-  system("ip addr");
-  copie(tunfd, 1);
-
-  return EXIT_SUCCESS;
-
-}
-*/
-/*
-int main (int argc, char** argv){
-
-  int tunfd;
-  printf("CrÃ©ation de %s\n",argv[1]);
-  tunfd = tun_alloc(argv[1]);
-  //printf("Faire la configuration de %s...\n",argv[1]);
-  //printf("Appuyez sur une touche pour continuer\n");
-  //getchar();
-  //printf("Interface %s ConfigurÃ©e:\n",argv[1]);
-  system("ip addr");
-  printf("\n\n");
-  char cmd[2048];
-  sprintf(cmd, "/mnt/partage/configure-tun.sh %s", "172.16.2.1/28");
-  printf("$: %s\n", cmd);
-
-  printf("\n\n");
-  system(cmd);
-  system("ip addr");
-  //copie(tunfd, 1);
-  printf("Appuyez sur une touche pour terminer\n");
-  getchar();
-
-  return 0;
-}*/
